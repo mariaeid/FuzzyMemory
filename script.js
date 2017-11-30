42b5917b9466a2b0f188a8f5b0b4b091577ae97b
root <root@DESKTOP-2TKMD5S.localdomain>
2017-11-30 21:37:53 +0100

    Eventlistener click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H',
 let memory_values = [];
 let memory_tile_id = [];
 let tiles_flipped = 0;
+let output = '';
 
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
@@ -14,24 +15,26 @@ function shuffle(a) {
 
 memory_array = shuffle(memory_array);
 
-// var boardDiv = document.createElement('div');
-// boardDiv.className = 'board';
-// document.getElementsByTagName('body')[0].appendChild(boardDiv);
 var boardDiv = document.querySelector('.board');
 
-memory_array.forEach(function(value) {
+memory_array.forEach(function(value, index) {
   let memory_array_value = value;
-
   var cardsDiv = document.createElement('div');
   cardsDiv.className = 'cards';
 
-  boardDiv.appendChild(cardsDiv);
-  console.log(memory_array_value);
+  output += 'id="tile_'+ index +'"' + boardDiv.appendChild(cardsDiv);
+  console.log(memory_array_value+' '+index);
+  let cardsFront = document.querySelectorAll('.cards');
+
+
+  cardFront.addEventListener('click', (event) => {
+    cardsDiv.innerHTML = memory_array_value;
+  });
 });
 
 function newGame() {
   tiles_flipped = 0;
   memory_array = shuffle(memory_array);
-  let output = '';
+
 
 }
